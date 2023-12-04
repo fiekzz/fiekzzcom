4afd3c23b3ae6595d8b3c71b6015f204b0371e7c
fiekzz <fikrichuck@gmail.com>
2023-12-05 03:15:11 +0800

    💅🏻 Postcard styling

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -3,6 +3,8 @@ interface PostCardContent {
   description: string;
   date: string;
   tags: string[];
+  index: number;
+  length: number;
 }
 
 export default function PostCard(props: PostCardContent) {
@@ -10,7 +12,7 @@ export default function PostCard(props: PostCardContent) {
     <div className="w-full rounded overflow-hidden">
       <div className="py-4">
         <div className="font-bold text-2xl mb-2">{props.title}</div>
-        <h6 className="text-gray-800 text-xl">{props.description}</h6>
+        <h6 className="text-gray-800 text-lg">{props.description}</h6>
         <h6 className="text-gray-600 text-base font-bold mt-2">{props.date}</h6>
       </div>
       <div className="pb-2">
@@ -25,6 +27,7 @@ export default function PostCard(props: PostCardContent) {
           );
         })}
       </div>
+      {props.index !== props.length - 1 ? 
       <hr
         className="rounded"
         style={{
@@ -33,6 +36,8 @@ export default function PostCard(props: PostCardContent) {
           marginBottom: "1rem",
         }}
       />
+       : null
+      }
     </div>
   );
 }
